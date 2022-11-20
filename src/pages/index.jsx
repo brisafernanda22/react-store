7de48b716954fe704a35934df44efe1450b34ec6
Richard Yordy <evil.darkman@hotmail.com>
2022-11-20 15:28:38 -0600

    view image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,9 +9,23 @@ const AgregarProducto = () => {
   const [nombre, setNombre] = useState('');
   const [precio, setPrecio] = useState('');
   const [categoria, setCategoria] = useState('');
+  const [file, setFile] = useState('Ningun archivo seleccionado');
 
   const handleSubmit = () =>{
     console.log('Datos que se estan agregando ', id);
+  };
+
+  const handleFile = async ({target}) =>{
+    
+    const reader = new FileReader();
+
+    reader.onload = ev => {
+      console.log('Data = >', ev.target.result);
+      setFile(ev.target.result);
+    }
+
+
+    reader.readAsDataURL(target.files[0]);
   };
 
   return (
@@ -34,7 +48,11 @@ const AgregarProducto = () => {
           <Input labelPlaceholder="categoria" onChange={(e)=>setCategoria(e.target.value)}/>
         </Col>
         <Col>
-          <Input labelPlaceholder="img" />
+          <Row>
+            {/* <Button light auto onClick={handleFile}>examinar</Button>
+            <Input labelPlaceholder="img" value={file} /> */}
+            <input type='file' onChange={handleFile} accept="image/*" />
+          </Row>
         </Col>
       </Row>
       <Spacer y={1} />
@@ -46,7 +64,7 @@ const AgregarProducto = () => {
       <div className='divider' />
       <Grid.Container gap={2} justify="center">
       <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' />
+        <CardImage id='1' title='example title' src={file} />
       </Grid>
       <Grid xs={12} sm={3}>
         <CardImage id='1' title='example title' />
