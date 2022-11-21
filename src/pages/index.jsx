60650ec3fc743b969adc0c3d7c8b422875aaaa03
Richard Yordy <evil.darkman@hotmail.com>
2022-11-20 18:32:34 -0600

    added submit and show multiple products

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,20 +10,18 @@ const AgregarProducto = () => {
   const [precio, setPrecio] = useState('');
   const [categoria, setCategoria] = useState('');
   const [file, setFile] = useState('Ningun archivo seleccionado');
+  const [allData, setAllData] = useState([]);
 
-  const handleSubmit = () =>{
-    console.log('Datos que se estan agregando ', id);
-  };
+  const handleSubmit = () => setAllData([...allData, { id, nombre, precio, categoria, file }]);
 
-  const handleFile = async ({target}) =>{
-    
+  const handleFile = async ({ target }) => {
+
     const reader = new FileReader();
 
     reader.onload = ev => {
       console.log('Data = >', ev.target.result);
       setFile(ev.target.result);
-    }
-
+    };
 
     reader.readAsDataURL(target.files[0]);
   };
@@ -33,19 +31,19 @@ const AgregarProducto = () => {
       <Spacer y={2} />
       <Row justify='center' gap={2} >
         <Col>
-          <Input labelPlaceholder="id" onChange={(e)=>setId(e.target.value)} />
+          <Input labelPlaceholder="id" onChange={(e) => setId(e.target.value)} />
         </Col>
         <Col>
-          <Input labelPlaceholder="Nombre" onChange={(e)=>setNombre(e.target.value)}/>
+          <Input labelPlaceholder="Nombre" onChange={(e) => setNombre(e.target.value)} />
         </Col>
       </Row>
       <Spacer y={1} />
       <Row>
         <Col>
-          <Input labelPlaceholder="precio" onChange={(e)=>setPrecio(e.target.value)}/>
+          <Input labelPlaceholder="precio" onChange={(e) => setPrecio(e.target.value)} />
         </Col>
         <Col>
-          <Input labelPlaceholder="categoria" onChange={(e)=>setCategoria(e.target.value)}/>
+          <Input labelPlaceholder="categoria" onChange={(e) => setCategoria(e.target.value)} />
         </Col>
         <Col>
           <Row>
@@ -57,29 +55,20 @@ const AgregarProducto = () => {
       </Row>
       <Spacer y={1} />
       <Row justify='center' >
-        <Button onClick={handleSubmit}>
+        <Button onPress={handleSubmit}>
           Agregar
         </Button>
       </Row>
       <div className='divider' />
       <Grid.Container gap={2} justify="center">
-      <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' src={file} />
-      </Grid>
-      <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' />
-      </Grid>
-      <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' />
-      </Grid>
-      <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' />
-      </Grid>
-      <Grid xs={12} sm={3}>
-        <CardImage id='1' title='example title' />
-      </Grid>
-    </Grid.Container>
-      
+        {allData.map(d => (
+          <Grid xs={12} sm={3}>
+            <CardImage id={d.id} title={d.name} src={d.file} />
+          </Grid>
+        ))}
+
+      </Grid.Container>
+
     </Container>
   )
 }
